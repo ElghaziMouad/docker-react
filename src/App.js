6a228b2093008d931e1ad87d019eb52deae958f9
ElghaziMouad <elghazi.mouad1@gmail.com>
2020-09-21 18:50:01 +0000

    changed app text 4

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Bye there,I was changed Learn React
+          Bye there,feature file
         </a>
       </header>
     </div>
